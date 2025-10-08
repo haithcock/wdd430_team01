047060102f5e8ddc873b32b0fedd319488aa44ea
nelsonpetro <nelsonpetrodoria@gmail.com>
2025-10-08 12:37:36 -0500

    catalog updates

--- my-next-app/src/app/(overview)/catalog/page.tsx
+++ my-next-app/src/app/(overview)/catalog/page.tsx
@@ -5,6 +5,8 @@ export const metadata: Metadata = {
   title: 'Handcrafted Haven- Catalog',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default function Catalog() {
   return (
     <div className="grid gap-8 justify-center grid-cols-[repeat(auto-fit,minmax(200px,300px))] py-32 bg-gray-50 px-8">
